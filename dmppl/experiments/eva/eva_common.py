af7b0e5ded8b75e5e0c9d5395afc281becfd88aa
Dave McEwan <dave.mcewan@bristol.ac.uk>
2019-10-17 11:37:45 +0100

    eva metric()

--- dmppl/experiments/eva/eva_common.py
+++ dmppl/experiments/eva/eva_common.py
@@ -1,5 +1,6 @@
 
 # Standard library imports
+from functools import partial
 import inspect
 import os
 
@@ -8,6 +9,9 @@ import toml
 
 # Local library imports
 from dmppl.base import Bunch, fnameAppendExt, verb, joinP
+from dmppl.fx import *
+from dmppl.math import powsineCoeffs
+from dmppl.nd import *
 
 __version__ = "0.1.0"
 
@@ -77,4 +81,25 @@ def loadEvcx(): # {{{
 
     return evcx
 # }}} def loadEvcx
+
+def metric(name, winSize, winAlpha, nBits=0): # {{{
+    '''Take attributes of a metric, return a callable implementation.
+
+    E.g. Use like: metric("foo")(x, y)
+    '''
+    w = powsineCoeffs(winSize, winAlpha)
+
+    assert 0 == nBits, "TODO: Implement fx*()"
+    fs = {
+        "Cex": partial(fxCex if 0 < nBits else ndCex, w, nBits=nBits),
+        "Cls": partial(fxCls if 0 < nBits else ndCls, w, nBits=nBits),
+        "Cos": partial(fxCos if 0 < nBits else ndCos, w, nBits=nBits),
+        "Cov": partial(fxCov if 0 < nBits else ndCov, w, nBits=nBits),
+        "Dep": partial(fxDep if 0 < nBits else ndDep, w, nBits=nBits),
+        "Ham": partial(fxHam if 0 < nBits else ndHam, w, nBits=nBits),
+        "Tmt": partial(fxTmt if 0 < nBits else ndTmt, w, nBits=nBits),
+    }
+
+    return fs[name]
+# }}} def metric
 

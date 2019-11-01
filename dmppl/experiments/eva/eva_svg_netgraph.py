14ebfbcdaa7edcb47902d9e270b78536ccfe87b3
Dave McEwan <dave.mcewan@bristol.ac.uk>
2019-11-01 19:19:24 +0000

    eva WIP Starting svgNetgraph()

--- dmppl/experiments/eva/eva_svg_netgraph.py
+++ dmppl/experiments/eva/eva_svg_netgraph.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # Local library imports
 from dmppl.base import dbg, info, verb
-from dmppl.math import ptsMkPolygon
+from dmppl.math import ptShift, ptsMkPolygon
 
 # Project imports
 # NOTE: Roundabout import path for eva_common necessary for unittest.
@@ -23,27 +23,65 @@ def svgNetgraph(u, cfg, vcdInfo, edges): # {{{
     measureNames = vcdInfo["unitIntervalVarNames"]
     nameParts = [eva.measureNameParts(nm) for nm in measureNames]
 
-    baseNames = set(bn for mt,st,bn in nameParts) # One sibGrp per base name.
+    baseNames = sorted(list(set(bn for mt,st,bn in nameParts))) # One sibGrp per base name.
 
     evs = eva.rdEvs(measureNames, u, u + cfg.windowsize, cfg.fxbits)
     expectation = eva.metric("Ex", cfg.windowsize, cfg.windowalpha, nBits=cfg.fxbits)
     exs = [expectation(evs[nm]) for nm in measureNames]
+    # TODO: background-color by Ex
 
-    mapSiblingTypeToOffset = {
+    # TODO: correct radius
+    sibGrpCenters = list(ptsMkPolygon(nPts=len(baseNames), radius=[100.0]))
+    globalCenters = [sibGrpCenters[baseNames.index(bn)] for mt,st,bn in nameParts]
+
+    mapSiblingTypeToLocalCenter = {
         "measure":    (0, 0),
         "reflection": (30, 0),
         "rise":       (0, 30),
         "fall":       (30, 30),
     }
-    localCenters = [mapSiblingTypeToOffset[st] for mt,st,bn in nameParts]
+    localCenters = [mapSiblingTypeToLocalCenter[st] for mt,st,bn in nameParts]
 
-    sibGrpCenters = ptsMkPolygon(nPts=len(baseNames)) # TODO: pair with full names.
+    assert len(globalCenters) == len(localCenters), \
+        (len(globalCenters), len(localCenters))
+    nodeCenters = [ptShift(gc, lc) for gc,lc in zip(globalCenters, localCenters)]
 
-    # TODO
+    nodeFmt = ' '.join((
+        '<g',
+            'id="%s"', # measureName
+            'class="node %s"',   # siblingType
+            'transform="translate(%f,%f)"', # centerX,centerY
+        '>',
+          '<circle/>', # Radius, center, style defined in CSS.
+          '<rect/>', # Width, height, x, y defined in CSS.
+          # TODO: identicon
+          # TODO: symbol
+        '</g>',
+    ))
+
+    # TODO: Sibling group internals
+
+    viewBoxWidth, viewBoxHeight = 1000, 1000 # TODO: correct viewBox
+
+    # 0,0 is top-left of 100x100 box
+    viewBoxMinX, viewBoxMinY = -500, -500 # TODO: correct viewBox
+
+    svgClassList = ["netgraph"]
+
     ret_ = []
     ret_.append('<div class="netgraph">')
-    ret_.append(  '<svg>')
-    ret_.append(    '<rect width="50" height="100">TODO</rect>')
+    ret_.append(' '.join((
+        '<svg',
+          'xmlns="http://www.w3.org/2000/svg"',
+          'xmlns:xlink="http://www.w3.org/1999/xlink"',
+          'viewBox="%d %d %d %d"' % (viewBoxMinX, viewBoxMinY, viewBoxWidth, viewBoxHeight),
+          'class="%s"' % ' '.join(svgClassList),
+          '>',
+    )))
+    ret_ += [nodeFmt % (nm, st, pt[0], pt[1]) \
+             for nm,(mt,st,bn),pt in zip(measureNames,
+                                         nameParts,
+                                         nodeCenters)]
     ret_.append(  '</svg>')
     ret_.append('</div>')
     return ret_

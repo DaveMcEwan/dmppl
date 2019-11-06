d9ac595562681c02665d4a0059813f15508929e0
Dave McEwan <dave.mcewan@bristol.ac.uk>
2019-11-06 13:52:45 +0000

    eva netgraph Move static strings to module level.

--- dmppl/experiments/eva/eva_svg_netgraph.py
+++ dmppl/experiments/eva/eva_svg_netgraph.py
@@ -18,7 +18,175 @@ from dmppl.color import rgb1D, rgb2D, identiconSpriteSvg
 from dmppl.experiments.eva.eva_common import paths, measureNameParts, metric, rdEvs, \
     mapSiblingTypeToHtmlEntity
 
+# {{{ Static format strings
+
+# Experimental option to cut down on complexity of SVG code by using CSS
+# properties.
+# Only works with SVG2 support, so older (not really that old) still require
+# attributes on each element.
 cssProps = False
+
+sibgrpSeparation = 100 # Space between groups of sibling nodes. HEURISTIC
+sibSeparation = 60 # Space between sibling nodes. HEURISTIC
+mapSiblingTypeToLocalCenter = { # heuristic
+    "measure":    (0, 0),
+    "reflection": (0, sibSeparation),
+    "rise":       (sibSeparation, 0),
+    "fall":       (sibSeparation, sibSeparation),
+}
+
+# Title provides mouseover information and should apply to all elements
+# representing a node.
+titleFmt = '''\
+<title>{measureName}
+
+E&#x0307; = {exValue}
+</title>'''
+
+# Blob (circular container) has background color representing E[x].
+blobX, blobY = 0, 0
+blobRadius = 25
+if cssProps:
+    blobFmt = '<circle class="node" fill="#{exRgb}"/>'
+else:
+    blobFmt = ' '.join((
+      '<circle',
+        ' class="node"',
+        ' cx="%d" cy="%d"' % (blobX, blobY),
+        ' r="%d"' % blobRadius,
+        ' style="stroke:lime; stroke-width:0.2; stroke-opacity:1;"',
+        ' fill="#{exRgb}"',
+      '/>'))
+
+# Tombstone (colored rectangle) is quick to recognise measurement type.
+tombstoneWidth, tombstoneHeight = 20, 20
+tombstoneX, tombstoneY = tombstoneWidth / -2, tombstoneHeight / -2
+if cssProps:
+    tombstoneFmt = '<rect class="node {measureType}"/>'
+else:
+    tombstoneFmt = ' '.join((
+      '<rect',
+        ' class="node {measureType}"',
+        ' rx="3px" ry="3px"',
+        ' width="%d" height="%d"' % (tombstoneWidth, tombstoneHeight),
+        ' x="%d" y="%d"' % (tombstoneX, tombstoneY),
+        ' style="fill:{tombstoneFill}"',
+      '/>'))
+
+# Symbol (text) to be vertically centered tombstone.
+symbolX, symbolY = -4, 4 # HEURISTIC
+if cssProps:
+    symbolFmt = '<text class="sym" x="%d" y="%d">{symbol}</text>' % \
+          (symbolX, symbolY)
+else:
+    symbolFmt = ' '.join((
+      '<text',
+        ' class="sym"',
+        ' x="%d" y="%d"'% (symbolX, symbolY),
+        ' style="font-size:15px;font-family:sans-serif;fill:{symbolFill};"',
+      '>{symbol}</text>'))
+
+# Node is the collection of all elements representing a measure.
+nodeFmt = ' '.join((
+    '<g',
+        'id="{measureName}"',
+        'class="node {siblingType}"',
+        'transform="translate({centerX},{centerY})"',
+    '>',
+      titleFmt,
+      blobFmt,
+      tombstoneFmt, # Tombstone
+      symbolFmt,
+    '</g>',
+))
+
+mapMeasureTypeToTombstoneFill = {
+    "event":     "white",
+    "bstate":    "blue",
+    "threshold": "red",
+    "normal":    "black",
+}
+
+mapMeasureTypeToSymbolFill = {
+    "event":     "#404040",
+    "bstate":    "lime",
+    "threshold": "lime",
+    "normal":    "lime",
+}
+
+
+identiconOffsetX, identiconOffsetY = 0, 0 # HEURISTIC
+identiconScale = 0.025
+identiconFmt = \
+    ('<g transform="translate({centerX},{centerY}) scale(%0.03f)">'
+     '{identiconSvg}'
+     '</g>') % identiconScale
+
+topStyle = '' if not cssProps else ' '.join((
+  '<style>',
+    'g.node > circle.node {',
+        'stroke:lime; stroke-width:0.2; stroke-opacity:1;',
+        'cx:%d; cy:%d;' % (blobX, blobY),
+        'r:%d;' % blobRadius,
+    '}',
+    'g.node > text.sym {',
+        'font-size:15px; font-family:sans-serif;',
+        'x:%d; y:%d;' % (symbolX, symbolY),
+    '}',
+    'g.node > rect.node {',
+        'rx:3px; ry:3px;',
+        'x:%d; y:%d;' % (tombstoneX, tombstoneY),
+        'width:%dpx; height:%dpx;' % (tombstoneWidth, tombstoneHeight),
+    '}',
+    'g.node > rect.event     { fill:%s; }' % mapMeasureTypeToTombstoneFill["event"],
+    'g.node > rect.bstate    { fill:%s; }' % mapMeasureTypeToTombstoneFill["bstate"],
+    'g.node > rect.threshold { fill:%s; }' % mapMeasureTypeToTombstoneFill["threshold"],
+    'g.node > rect.normal    { fill:%s; }' % mapMeasureTypeToTombstoneFill["normal"],
+    'rect.event     ~ text.sym { fill:%s; }' % mapMeasureTypeToSymbolFill["event"],
+    'rect.bstate    ~ text.sym { fill:%s; }' % mapMeasureTypeToSymbolFill["bstate"],
+    'rect.threshold ~ text.sym { fill:%s; }' % mapMeasureTypeToSymbolFill["threshold"],
+    'rect.normal    ~ text.sym { fill:%s; }' % mapMeasureTypeToSymbolFill["normal"],
+  '</style>',
+))
+
+svgRootFmt = ' '.join((
+  '<svg',
+    'xmlns="http://www.w3.org/2000/svg"',
+    'xmlns:xlink="http://www.w3.org/1999/xlink"',
+    'xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"',
+    'xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"',
+    'class="netgraph"',
+    'viewBox="{viewBoxMinX} {viewBoxMinY} {viewBoxWidth} {viewBoxHeight}"',
+    'width="{svgWidth}mm"',
+    'height="{svgHeight}mm"',
+  '>',
+))
+sodipodiNamedview = ' '.join((
+  '<sodipodi:namedview',
+    'id="base"',
+    'pagecolor="#ffffff"',
+    'bordercolor="#666666"',
+    'borderopacity="1.0"',
+    'showgrid="true"',
+    'borderlayer="false"',
+    'objecttolerance="10000"',
+    'inkscape:pageopacity="0.0"',
+    'inkscape:pageshadow="2"',
+    'inkscape:document-units="mm"',
+    'inkscape:current-layer="layer1"',
+    'inkscape:pagecheckerboard="false"',
+    'inkscape:showpageshadow="false">',
+    '<inkscape:grid',
+      'type="xygrid"',
+      'units="mm"',
+      'spacingx="1"',
+      'spacingy="1"',
+      'dotted="false"',
+    '/>',
+  '</sodipodi:namedview>',
+))
+
+# }}} Static format strings
 
 def calculateEdges(f, g, u, x, y,
                    cfg, dsfDeltas, vcdInfo): # {{{
@@ -37,7 +205,6 @@ def svgNodes(exs): # {{{
 
     # {{{ nodes
 
-    sibgrpSeparation = 100 # Space between groups of sibling nodes. HEURISTIC
     sibgrpRadius = (len(measureNames) * sibgrpSeparation) / (2 * pi)
 
     sibgrpCenters = \
@@ -47,15 +214,6 @@ def svgNodes(exs): # {{{
         {nm: sibgrpCenters[mapBaseNameToSibgrpIdx[bn]] \
          for nm,(mt,st,bn) in zip(measureNames, nameParts)}
 
-
-    sibSeparation = 60 # Space between sibling nodes. HEURISTIC
-    mapSiblingTypeToLocalCenter = { # heuristic
-        "measure":    (0, 0),
-        "reflection": (0, sibSeparation),
-        "rise":       (sibSeparation, 0),
-        "fall":       (sibSeparation, sibSeparation),
-    }
-
     nodeLocalCenters = \
         {nm: mapSiblingTypeToLocalCenter[st] \
          for nm,(mt,st,bn) in zip(measureNames, nameParts)}
@@ -64,105 +222,24 @@ def svgNodes(exs): # {{{
         {nm: ptShift(nodeSibgrpCenters[nm], nodeLocalCenters[nm]) \
          for nm in measureNames}
 
-
-    # Title provides mouseover information and should apply to all elements
-    # representing a node.
-    titleFmt = '''\
-<title>{measureName}
-
-E&#x0307; = {exValue}
-</title>'''
-
-    # Blob (circular container) has background color representing E[x].
-    blobX, blobY = 0, 0
-    blobRadius = 25
-    if cssProps:
-        blobFmt = '<circle class="node" fill="#{exRgb}"/>'
-    else:
-        blobFmt = ('<circle'
-            ' class="node"'
-            ' cx="%d" cy="%d"'
-            ' r="%d"'
-            ' style="stroke:lime; stroke-width:0.2; stroke-opacity:1;"'
-            ' fill="#{exRgb}"'
-            '/>') % \
-            (blobX, blobY, blobRadius)
-
-    # Tombstone (colored rectangle) is quick to recognise measurement type.
-    tombstoneWidth, tombstoneHeight = 20, 20
-    tombstoneX, tombstoneY = tombstoneWidth / -2, tombstoneHeight / -2
-    if cssProps:
-        tombstoneFmt = '<rect class="node {measureType}"/>'
-    else:
-        tombstoneFmt = ('<rect'
-            ' class="node {measureType}"'
-            ' rx="3px" ry="3px"'
-            ' width="%d" height="%d"'
-            ' x="%d" y="%d"'
-            ' style="fill:{tombstoneFill}"'
-            '/>') % \
-            (tombstoneWidth, tombstoneHeight, tombstoneX, tombstoneY)
-
-    # Symbol (text) to be vertically centered in RHS of tombstone.
-    symbolOffsetX, symbolOffsetY = -4, 4 # HEURISTIC
-    if cssProps:
-        symbolFmt = '<text class="sym" x="%d" y="%d">{symbol}</text>' % \
-              (symbolOffsetX, symbolOffsetY)
-    else:
-        symbolFmt = ('<text'
-            ' class="sym"'
-            ' x="%d" y="%d"'
-            ' style="font-size:15px;font-family:sans-serif;fill:{symbolFill};">'
-            '{symbol}'
-            '</text>') % \
-            (symbolOffsetX, symbolOffsetY)
-
-    # Node is the collection of all elements representing a measure.
-    nodeFmt = ' '.join((
-        '<g',
-            'id="{measureName}"',
-            'class="node {siblingType}"',
-            'transform="translate({centerX},{centerY})"',
-        '>',
-          titleFmt,
-          blobFmt,
-          tombstoneFmt, # Tombstone
-          symbolFmt,
-        '</g>',
-    ))
-
-    mapMeasureTypeToTombstoneFill = {
-        "event":     "white",
-        "bstate":    "blue",
-        "threshold": "red",
-        "normal":    "black",
-    }
-
-    mapMeasureTypeToSymbolFill = {
-        "event":     "#404040",
-        "bstate":    "lime",
-        "threshold": "lime",
-        "normal":    "lime",
-    }
-
-    nodes = (nodeFmt.format(measureName=nm,
-                          siblingType=st,
-                          measureType=mt,
-                          baseName=bn,
-                          centerX=nodeCenters[nm][0],
-                          centerY=nodeCenters[nm][1],
-                          symbolFill=mapMeasureTypeToSymbolFill[mt],
-                          tombstoneFill=mapMeasureTypeToTombstoneFill[mt],
-                          exRgb=rgb1D(exs[nm]),
-                          exValue=exs[nm],
-                          symbol=mapSiblingTypeToHtmlEntity[st]) \
-           for nm,(mt,st,bn) in zip(measureNames, nameParts))
+    nodes = \
+        (nodeFmt.format(measureName=nm,
+                        siblingType=st,
+                        measureType=mt,
+                        baseName=bn,
+                        centerX=nodeCenters[nm][0],
+                        centerY=nodeCenters[nm][1],
+                        symbolFill=mapMeasureTypeToSymbolFill[mt],
+                        tombstoneFill=mapMeasureTypeToTombstoneFill[mt],
+                        exRgb=rgb1D(exs[nm]),
+                        exValue=exs[nm],
+                        symbol=mapSiblingTypeToHtmlEntity[st]) \
+         for nm,(mt,st,bn) in zip(measureNames, nameParts))
 
     # }}} nodes
 
     # {{{ identicons
     # One per sibling group.
-
     identiconRadius = sibgrpRadius + 1.5*sibgrpSeparation
 
     _identiconCenters = \
@@ -170,14 +247,6 @@ E&#x0307; = {exValue}
     identiconCenters = \
         {bn: _identiconCenters[mapBaseNameToSibgrpIdx[bn]] \
          for bn in _baseNames}
-
-    # Identicon to be vertically centered in LHS of tombstone.
-    identiconOffsetX, identiconOffsetY = 0, 0 # HEURISTIC
-    identiconScale = 0.025
-    identiconFmt = \
-        ('<g transform="translate({centerX},{centerY}) scale(%0.03f)">'
-         '{identiconSvg}'
-         '</g>') % identiconScale
 
     # Identicons are embedded SVGs scaled and translated into place.
     identiconSvgs = \
@@ -215,60 +284,16 @@ def svgNetgraph(u, cfg, vcdInfo, edges): # {{{
         svgWidth, svgHeight
 
     ret_ = []
-    ret_.append(' '.join((
-      '<svg',
-        'xmlns="http://www.w3.org/2000/svg"',
-        'xmlns:xlink="http://www.w3.org/1999/xlink"',
-        'xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"',
-        'xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"',
-        'class="netgraph"',
-        'viewBox="%d %d %d %d"' % (viewBoxMinX, viewBoxMinY, viewBoxWidth, viewBoxHeight),
-        'width="%dmm"' % svgWidth,
-        'height="%dmm"' % svgHeight,
-      '>',
-    )))
-    ret_.append(' '.join((
-      '<sodipodi:namedview',
-        'id="base"',
-        'pagecolor="#ffffff"',
-        'bordercolor="#666666"',
-        'borderopacity="1.0"',
-        'showgrid="true"',
-        'borderlayer="false"',
-        'objecttolerance="10000"',
-        'inkscape:pageopacity="0.0"',
-        'inkscape:pageshadow="2"',
-        'inkscape:document-units="mm"',
-        'inkscape:current-layer="layer1"',
-        'inkscape:pagecheckerboard="false"',
-        'inkscape:showpageshadow="false">',
-        '<inkscape:grid',
-          'type="xygrid"',
-          'units="mm"',
-          'spacingx="1"',
-          'spacingy="1"',
-          'dotted="false"',
-        '/>',
-      '</sodipodi:namedview>',
-    )))
+    ret_.append(svgRootFmt.format(svgWidth=svgWidth,
+                               svgHeight=svgHeight,
+                               viewBoxMinX=viewBoxMinX,
+                               viewBoxMinY=viewBoxMinY,
+                               viewBoxWidth=viewBoxWidth,
+                               viewBoxHeight=viewBoxHeight))
 
-    if cssProps:
-        ret_.append(' '.join((
-          '<style>',
-            'g.node > circle.node { cx:0; cy:0; r:25; stroke:lime; stroke-width:0.2; stroke-opacity:1; }',
-            'g.node > text.sym { x:0; y:0; font-size:15px; font-family:sans-serif; }',
-            'g.node > rect.node { x:-10; y:-10; height:20px; width:20px; rx:3px; ry:3px; }',
-            'g.node > rect.event { fill:white; }',
-            'g.node > rect.bstate { fill:blue; }',
-            'g.node > rect.threshold { fill:red; }',
-            'g.node > rect.normal { fill:black; }',
-            'g.node > text.sym.measure { fill:lime; }',
-            'rect.event ~ text.sym { fill:#404040; }',
-            'rect.bstate ~ text.sym { fill:lime; }',
-            'rect.threshold ~ text.sym { fill:lime; }',
-            'rect.normal ~ text.sym { fill:lime; }',
-          '</style>',
-        )))
+    ret_.append(sodipodiNamedview)
+
+    ret_.append(topStyle)
 
     # Force background to white
     ret_.append('<rect width="100%%" height="100%%" fill="white" x="%d" y="%d"/>' \

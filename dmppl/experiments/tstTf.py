4b7f31a5e80c13806282cd058ce67134f6d39194
Dave McEwan <dave.mcewan@bristol.ac.uk>
2019-11-18 19:00:43 +0000

    experiments/tstTf.py day2 of playing with relest+tf
    customLoss goes to NaN which destroys model.

--- dmppl/experiments/tstTf.py
+++ dmppl/experiments/tstTf.py
@@ -1,77 +1,177 @@
 from __future__ import absolute_import, division, print_function
 
+from datetime import datetime
 import tensorflow as tf
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Dense
+import os
 
-defaultColumns = [
-    "known",
-    "E[X]",
-    "E[Y]",
-    "E[X*Y]",
-    "E[|X-Y|]",
-    #"Cos(X,Y)",
-]
+from dmppl.stats import *
 
-# https://www.tensorflow.org/tutorials/load_data/csv
-def getDataset(fname, **kwargs): # {{{
+def getDatasets(): # {{{
+    # https://www.tensorflow.org/tutorials/load_data/csv
+
+    selectColumns = [
+        "known",
+        "E[X]",
+        "E[Y]",
+        "E[X*Y]",
+        "E[|X-Y|]",
+        "Cls(X,Y)",
+        "Cos(X,Y)",
+        "Cov(X,Y)",
+        "Dep(X,Y)",
+        "Ham(X,Y)",
+        "Tmt(X,Y)",
+    ]
 
     # NOTE: make_csv_dataset is experimental. Look out for API change.
     # NOTE: Reader is very strict! Can't cope with multi-char delimiter.
-    return tf.data.experimental.make_csv_dataset(
-        fname,
-        batch_size=256,
+
+    raw_dataset_train = tf.data.experimental.make_csv_dataset(
+        "combinedTrain.csv",
+        batch_size=64,
         label_name="known", # NOTE: Must be in `select_columns`
-        select_columns=kwargs.get("columns", defaultColumns),
+        select_columns=selectColumns,
         shuffle=True,
+        sloppy=True, # Sloppy --> Non-deterministic
+        num_epochs=-1, # Infinitely repeating dataset
+    )
+
+    raw_dataset_test = tf.data.experimental.make_csv_dataset(
+        "combinedTest.csv",
+        batch_size=64,
+        label_name="known", # NOTE: Must be in `select_columns`
+        select_columns=selectColumns,
+        shuffle=False,
         sloppy=False, # Non-sloppy --> deterministic
         num_epochs=1, # Not infinitely repeating dataset
     )
-# }}} def getDataset
 
-def showBatch(batch): # {{{
-    for k, v in batch.items():
-        print("{:20s}: {}".format(k, v.numpy()))
-# }}} def showBatch
+    def showBatch(batch): # {{{
+        for k, v in batch.items():
+            print("{:20s}: {}".format(k, v.numpy()))
+    # }}} def showBatch
 
-datasetTrain = getDataset("combined.csv")
-datasetTest = getDataset("combined.csv") # TODO: Split these
+    def pack(features, label): # {{{
+        return tf.stack(list(features.values()), axis=-1), label
+    # }}} def pack
 
-# TODO: Pack items into inputs
-def pack(features, label): # {{{
-    return tf.stack(list(features.values()), axis=-1), label
-# }}} def pack
+    packed_dataset_train = raw_dataset_train.map(pack)
+    packed_dataset_test = raw_dataset_test.map(pack)
 
-packedDatasetTrain = datasetTrain.map(pack)
-packedDatasetTest = datasetTest.map(pack)
+    # Do additional shuffling here.
+    dataset_train = packed_dataset_train
+    dataset_test = packed_dataset_test
 
-# nHidden ∈ {0, 2..5}
-# act ∈ {sigmoid, relu, tanh} # TODO: hard_sigmoid instead of sigmoid?
-nHidden1 = 0
-nHidden2 = 0
-act1 = "hard_sigmoid"
-act2 = "relu"
-modelName = "{act1}{nHidden1}_{act2}{nHidden2}".format(
-    act1=act1 if 0 < nHidden1 else "",
-    nHidden1=nHidden1 if 0 < nHidden1 else "NONE",
-    act2=act2 if 0 < nHidden2 else "",
-    nHidden2=nHidden2 if 0 < nHidden2 else "NONE",
-)
+    nInputs = len(selectColumns)-1
 
-assert nHidden1 >= nHidden2
+    return nInputs, dataset_train, dataset_test
+# }}} def getDatasets
 
-inputs = tf.keras.Input(shape=(len(defaultColumns)-1,))
-hidden1 = tf.keras.layers.Dense(nHidden1, activation=act1)(inputs)
-hidden2 = tf.keras.layers.Dense(nHidden2, activation=act2)(hidden1)
-outputs = tf.keras.layers.Dense(1, activation="hard_sigmoid") \
-    (hidden2 if 0 < nHidden2 else (hidden1 if 0 < nHidden1 else inputs))
+def buildModel(nInputs): # {{{
+    # nHidden ∈ {0, 2..5}
+    nHidden1, nHidden2 = 8, 8
 
-model = tf.keras.Model(inputs=inputs, outputs=outputs, name=modelName)
+    # act ∈ {sigmoid, relu, tanh}
+    activation1, activation2 = "hard_sigmoid", "hard_sigmoid"
 
-model.summary()
-tf.keras.utils.plot_model(model, modelName+".png", show_shapes=True)
+    useHidden1, useHidden2 = (0 < nHidden1), (0 < nHidden2)
 
-modelLoss = "binary_crossentropy"
-modelOptimizer = "adam"
-modelMetrics = ["accuracy"]
-model.compile(loss=modelLoss, optimizer=modelOptimizer, metrics=modelMetrics)
+    modelName = "{a1}{n1}_{a2}{n2}".format(
+        a1=activation1 if useHidden1 else "",
+        n1=nHidden1 if useHidden1 else "I",
+        a2=activation2 if useHidden2 else "",
+        n2=nHidden2 if useHidden2 else "I",
+    )
 
-model.fit(packedDatasetTrain, epochs=20)
+    inputs = tf.keras.Input(shape=(nInputs,))
+    hidden1 = tf.keras.layers.Dense(nHidden1, activation=activation1)(inputs)
+    hidden2 = tf.keras.layers.Dense(nHidden2, activation=activation2)(hidden1)
+    outputs = tf.keras.layers.Dense(1, activation="hard_sigmoid") \
+        (hidden2 if useHidden2 else (hidden1 if useHidden1 else inputs))
+
+    ret = tf.keras.Model(inputs=inputs, outputs=outputs, name=modelName)
+
+    #ret.summary()
+    #tf.keras.utils.plot_model(ret, modelName+".png", show_shapes=True)
+
+    return ret
+# }}} def buildModel
+
+def fitModel(model, dataset): # {{{
+
+    def customLoss(y_true, y_pred): # {{{
+        '''Optimise for BMI and MCC.
+
+        TODO: Should not return NaN.
+        '''
+        y_true = tf.keras.backend.cast_to_floatx(y_true)
+        y_pred = tf.keras.backend.cast_to_floatx(y_pred)
+
+        def truePositive(y_true, y_pred):
+            return tf.keras.backend.sum(y_true * y_pred)
+
+        def falsePositive(y_true, y_pred):
+            return tf.keras.backend.sum((1-y_true) * y_pred)
+
+        def falseNegative(y_true, y_pred):
+            return tf.keras.backend.sum(y_true * (1-y_pred))
+
+        def trueNegative(y_true, y_pred):
+            return tf.keras.backend.sum((1-y_true) * (1-y_pred))
+
+        tp, fp, tn, fn = \
+            truePositive(y_pred, y_true), \
+            falsePositive(y_pred, y_true), \
+            trueNegative(y_pred, y_true), \
+            falseNegative(y_pred, y_true)
+
+        gain = bookmakersInformedness(tp, fp, fn, tn) #*  matthewsCorrelation(tp, fp, fn, tn)
+        return gain * -1
+    # }}} def customLoss
+
+    fitLoss = "binary_crossentropy"
+    #fitLoss = customLoss
+
+    fitOptimizer = tf.keras.optimizers.Adam(learning_rate=0.001, amsgrad=True, clipnorm=1.0, clipvalue=1.0)
+    #fitOptimizer = tf.keras.optimizers.Nadam()
+    #fitOptimizer = tf.keras.optimizers.Adadelta()
+    #fitOptimizer = tf.keras.optimizers.SGD()
+
+    fitMetrics = [
+        "acc", # accuracy
+        "mse", # mean squared error
+    ]
+
+    fitCallbacks = [
+        #tf.keras.callbacks.TensorBoard(log_dir=os.sep.join(("tf_logs", datetime.now().strftime("%Y%m%d-%H%M%S")))),
+    ]
+
+    model.compile(loss=fitLoss, optimizer=fitOptimizer, metrics=fitMetrics)
+
+    fitEpochs = 20
+    fitStepsPerEpoch = 100
+
+    model.fit(dataset,
+              epochs=fitEpochs,
+              steps_per_epoch=fitStepsPerEpoch,
+              callbacks=fitCallbacks)
+# }}} def fitModel
+
+nInputs, dataset_train, dataset_test = getDatasets()
+
+model = buildModel(nInputs)
+
+fitModel(model, dataset_train)
+
+#print(type(dataset_test))
+#print(list(dataset_test)[0])
+#print(list(dataset_test)[1])
+
+predictions = model.predict(dataset_test)
+nPred = 20
+for e, k in zip(predictions[:nPred], list(dataset_test)[0][1][:nPred]):
+    print(int(k), e[0])
+
+print('EVAL: loss={:0.03f}, acc={:0.03f}, mse={:0.03f}'.format(*model.evaluate(dataset_test)))

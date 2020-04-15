23cec21f5f97291b26810a51ef2ef7fe2da11299
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-04-15 18:02:26 +0100

    color CursesWindow

--- dmppl/color.py
+++ dmppl/color.py
@@ -3,8 +3,10 @@ from __future__ import absolute_import
 from __future__ import division
 
 import curses
+import functools
 import hashlib
 import itertools
+import locale
 import math
 import numpy as np
 import sys
@@ -232,6 +234,68 @@ def cursesInitPairs(): # {{{
         if f != b:
             curses.init_pair(n, f, b)
 # }}} def cursesInitPairs
+
+class CursesWindow(object): # {{{
+    '''Helper class to pre-calulate some constants and functions.
+    The window is a rectangle in the middle of the screen.
+    A box uses the first/last character position on each line/char, so
+    topLeft is the position of the box corner.
+
+    self.draw() is a wrapper for self.win.addstr() with encoding and color.
+
+    +-----------...
+    | Title...
+    |
+    ...
+    |
+    | Status...
+    +-----------...
+    '''
+    def __init__(self, scr, nLines=40, nChars=80, colorPair=0,
+                 beginY=None, beginX=None, center=True):
+
+        scrNLines, scrNChars = scr.getmaxyx()
+
+        assert isinstance(nLines, int), (type(nLines), nLines)
+        assert isinstance(nChars, int), (type(nChars), nChars)
+        self.nLines, self.nChars = nLines, nChars
+
+        if beginY is None:
+            if center:
+                self.beginY = (scrNLines - nLines) // 2
+            else:
+                self.beginY = 0
+        else:
+            assert isinstance(beginY, int), (type(beginY), beginY)
+            self.beginY = beginY
+
+        if beginX is None:
+            if center:
+                self.beginX = (scrNChars - nChars) // 2
+            else:
+                self.beginX = 0
+        else:
+            assert isinstance(beginX, int), (type(beginX), beginX)
+            self.beginX = beginX
+
+        self.linesHeight, self.charsWidth = nLines - 2, nChars - 2
+        self.lineTop, self.lineBottom = 1, nLines - 2
+        self.charLeft, self.charRight = 1, nChars - 2
+
+        self.win = scr.derwin(nLines, nChars, self.beginY, self.beginX)
+
+        self.drawStr = functools.partial(self._drawStr,
+                                         colorPair=colorPair)
+
+    def _drawStr(self, s, x=1, y=1, colorPair=0,
+                 encoding=locale.getpreferredencoding()): # {{{
+        '''Intended to be used with functools.partial()
+        '''
+        b = s.encode(encoding)
+        return self.win.addstr(y, x, b, curses.color_pair(colorPair))
+    # }}} def _drawStr
+
+# }}} class CursesWindow
 
 
 if __name__ == "__main__":

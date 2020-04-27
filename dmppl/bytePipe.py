601b0da3989051fc03dab07fbf00d3b88650b6bf
Dave McEwan <dave.mcewan@bristol.ac.uk>
2020-04-27 18:27:58 +0100

    bytePipe WIP bpReadPoll, bpReadBurst

--- dmppl/bytePipe.py
+++ dmppl/bytePipe.py
@@ -109,6 +109,61 @@ def bpWriteSequential(device, addrValues:BpAddrValues) -> BpAddrValues: # {{{
     return ret
 # }}} def bpWriteSequential
 
+def bpReadPoll(device, addrs:BpAddrs) -> Iterator[BpAddrValue]: # {{{
+
+    cyclicAddrs = itertools.cycle(addrs)
+    for i,addr in enumerate(addrs):
+
+        # Send read command
+        assert isinstance(addr, int), (type(addr), addr)
+        assert 0 <= addr < 128, (i, addr)
+        assert 1 == device.write(bytes([addr]))
+
+        value:int = ord(device.read(1))
+
+        # First return value is discarded.
+        if 0 == i:
+            continue
+        else:
+            ret:BpAddrValue = (addrs[i-1], value)
+            yield ret
+
+# }}} def bpReadPoll
+
+def bpReadBurst(device, addr:int, burstSize:int) -> BpValues: # {{{
+    '''Perform a burst read of up to 255 bytes from the same location.
+
+    It is intended that some locations are backed by a FIFO, so this allows the
+    bandwidth of the underlying channel (USB, RS232, I2C, SPI, etc) to be used
+    efficiently.
+    The underlying channel is assumed to produce data in quantites of whole
+    bytes.
+    '''
+
+    assert isinstance(burstSize, int), (type(burstSize), burstSize)
+    assert 1 <= burstSize <= 255
+
+    # Initialize burst downcounter.
+    addrValue0:BpAddrValue = bpWriteSequential(device, [(0, burstSize)])[0]
+    value0:BpValue = addrValue0[1]
+
+    # Send read command.
+    assert isinstance(addr, int), (type(addr), addr)
+    assert 0 <= addr < 128, addr
+    assert 1 == device.write(bytes([addr]))
+
+    # Retreive bytes.
+    bs = device.read(burstSize)
+    assert (burstSize+1) == len(bs), (burstSize, len(bs), bs)
+    assert value0 == ord(bs[0])
+
+    # Drop the first byte which is the config/status value at address 0.
+    ret = tuple([ord(b) for b in bs][1:])
+    assert burstSize == len(ret), (burstSize, len(ret), ret)
+
+    return ret
+# }}} def bpReadBurst
+
 def bpPrintMem(title:str, mem:BpMem) -> None: # {{{
 
     print(title.strip() + ':')

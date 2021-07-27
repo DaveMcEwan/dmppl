68e8a121d4591360080cd40121add1796ae48a1b
Dave McEwan <dave.mcewan@bristol.ac.uk>
2021-07-27 13:27:40 +0100

    pyproject bugfix Exception on finding entryPoint.

--- dmppl/__init__.py
+++ dmppl/__init__.py
@@ -1,12 +1,35 @@
 
+from os import path
 import sys
 
 import toml
 
-version = toml.load("pyproject.toml")["project"]["version"]
 
 # Don't write .pyc or .pyo files unless it's a release.
-if 0 != int(version.split('.')[-1]):
+# If pyproject.toml can't be found, then assume it's a release.
+try:
+    _parentDir = path.dirname(path.abspath(path.dirname(__file__)))
+    _pyproject = path.join(_parentDir, "pyproject.toml")
+    version = toml.load(_pyproject)["project"]["version"]
+
+    # https://semver.org/
+    # 9. A pre-release version MAY be denoted by appending a hyphen and a
+    # series of dot separated identifiers immediately following the patch
+    # version.
+    # Identifiers MUST comprise only ASCII alphanumerics and hyphens
+    # [0-9A-Za-z-]. Identifiers MUST NOT be empty.
+    # Numeric identifiers MUST NOT include leading zeroes.
+    # Pre-release versions have a lower precedence than the associated normal
+    # version.
+    # A pre-release version indicates that the version is unstable and might
+    # not satisfy the intended compatibility requirements as denoted by its
+    # associated normal version.
+    # Examples: 1.0.0-alpha, 1.0.0-alpha.1, 1.0.0-0.3.7, 1.0.0-x.7.z.92
+    if version.find('-') < 0:
+        raise
+    else:
+        pass
+except:
     sys.dont_write_bytecode = True
 
 # NOTE: __all__ is *not* defined in order to avoid namespace conflicts with
